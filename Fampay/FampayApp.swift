6aae40328c0a161b2baba46b2e66eb2eca1b8dec
yashsomkuwar <yash@getswipe.in>
2024-02-08 19:25:40 +0530

    Initial Commit

---
+++ Fampay/FampayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FampayApp.swift
+//  Fampay
+//
+//  Created by Yash Somkuwar on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FampayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
